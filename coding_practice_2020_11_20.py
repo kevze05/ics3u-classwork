649a2595bbce3aa268da4b287388590f65a762f3
kevze05 <57277295+kevze05@users.noreply.github.com>
2020-11-20 13:07:24 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -3,4 +3,10 @@
 colour = input("Hello, what is your favorite colour? ")
 print(f"Hey, {colour} is my favorite colour too!")
 
+#Question 2
 
+cans = int(input("How many cans are in each package? "))
+packages = int(input("How many total packages? "))
+
+total = cans*packages
+print(f"There are {total} cans in total.")
