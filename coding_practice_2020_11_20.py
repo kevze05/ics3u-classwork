1ee0b1dffc8329975a8f74e77afcad507d213ff5
kevze05 <57277295+kevze05@users.noreply.github.com>
2020-11-20 13:24:49 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -32,4 +32,4 @@ while (True):
         print("Ok. Good.")
         break
     else:
-        inp = input("Invalid input, please try again. (yes/no)")
+        inp = input("Invalid input, please try again. (yes/no) ")
