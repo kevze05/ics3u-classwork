22a05133a57ff3013ee4396312f8001983813493
kevze05 <57277295+kevze05@users.noreply.github.com>
2020-11-25 14:23:41 -0500

    Update coding_practice_2020_11_25.py

--- coding_practice_2020_11_25.py
+++ coding_practice_2020_11_25.py
@@ -62,3 +62,93 @@ def first_two(str):
 
 #Python Workbook if_else
 #------------------------
+
+#Exercise 34: Even or Odd?
+
+integer = int(input("Enter an integer: "))
+if (integer % 2 == 0):
+    print(f"{integer} is even")
+else:
+    print(f"{integer} is odd")
+
+#Exercise 35: Dog Years
+
+years = int(input("How old is your dog in human years? "))
+
+if (years < 0):
+    print("Error, negative age.")
+elif (years <= 2):
+    print(f"Your dog is {years*10.5} dog years old.")
+else:
+    print(f"Your dog is {21+(years-2)*4} dog years old.")
+
+#Exercise 36: Vowel or Consonant
+
+letter = input("Enter a character: ")
+
+vowels = ['a','e','i','o','u']
+if ((letter not in vowels) == False):
+    print(f"{letter} is a vowel.")
+elif (letter == 'y'):
+    print(f"Sometimes {letter} is a vowel and sometimes {letter} is a consonant.")
+else:
+    print(f"{letter} is a consonant.")
+
+#Exercise 37: Name that Shape
+
+sides = int(input("How many sides does your shape have? (3-10) "))
+
+vowels = ["Triangle", "Square", "Penta", "Hexa", "Hepta", "Octa", "Nona", "Deca"]
+
+if (sides < 3 or sides > 10):
+    print("Invalid input.")
+elif (sides < 5):
+    print(f"Your shape is a {vowels[sides-3]}.")
+else:
+    print(f"Your shape is a {vowels[sides-3]}gon.")
+
+#Exercise 38: Month Name to Number of Days
+
+month = input("What month is it? ")
+
+big = ["January", "March", "May", "July", "August", "October", "December"]
+
+if (month == "February"):
+    print(f"{month} has 28 or 29 days.")
+elif (month not in big):
+    print(f"{month} has 30 days.")
+else:
+    print(f"{month} has 31 days.")
+    
+#Exercise 39: Sound levels
+
+sound = int(input("How many decibels is the sound level? "))
+
+lb = -1
+ub = -1
+
+bench = ["Nothing", "Quiet room", "Alarm clock", "Gas lawnmower", "Jackhammer", "Way too loud"]
+
+tf = False
+
+if (sound < 0):
+    print("The sound level is Nothing.")
+    tf = True
+elif (sound <= 40):
+    lb = 0
+    ub = 1
+elif (sound <= 70):
+    lb = 1
+    ub = 2
+elif (sound <= 106):
+    lb = 2
+    ub = 3
+elif (sound <= 130):
+    lb = 3
+    ub = 4
+else:
+    print("The sound level is Way too loud.")    
+    tf = True
+
+if (tf == False):
+    print(f"The sound level is between {bench[lb]} and {bench[ub]}.")
