06c0949544989d9ef64e6488d4a892bf9ed79b28
kevze05 <57277295+kevze05@users.noreply.github.com>
2020-12-03 20:09:50 -0500

    Update coding_practice_2020_12_03.py

--- coding_practice_2020_12_03.py
+++ coding_practice_2020_12_03.py
@@ -31,3 +31,30 @@ def largest3(a,b,c):
 def first2(str):
     return str[0:2]
 
+
+num1 = 4
+num2 = 5
+num3 = 6
+total = add3(num1, num2, num3)
+print(f"The sum {num1}, {num2}, and {num3} is {total}.")
+
+message = print_message("Bob", 15)
+print(message)
+
+num1 = 10
+num2 = 20
+avg = average(num1, num2)
+print(f"The average of {num1} and {num2} is {avg}")
+
+num1 = 7
+num2 = 10
+num3 = 15
+big = largest3(num1, num2, num3)
+print(f"The largest out of {num1}, {num2}, and {num3} is {big}.")
+
+str = "Hello World"
+
+f2 = first2(str)
+
+print(f"The first two characters in {str} is {f2}.")
+
