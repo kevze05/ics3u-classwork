c8aa321e31e7f4fa27a256d264b2f9060301c380
kevze05 <57277295+kevze05@users.noreply.github.com>
2021-01-06 09:40:52 -0500

    Update coding_practice_2021_01_06.py

--- coding_practice_2021_01_06.py
+++ coding_practice_2021_01_06.py
@@ -29,3 +29,45 @@ def main():
 if __name__ == "__main__":
     main()
     #tests()
+
+#ASSERTION PRACTICE USING CODINGBAT TESTS
+
+def first_last6(li):
+    if li[0] == 6 or li[-1] == 6:
+        return True
+    else:
+        return False
+
+def same_first_last(li):
+    if (len(li) == 0):
+        return False
+    elif li[0] == li[-1]:
+        return True
+    else:
+        return False
+
+def test_same_first_last():
+    assert same_first_last([1, 2, 3]) == False
+    assert same_first_last([1, 2, 3, 1]) == True
+    assert same_first_last([1, 2, 1]) == True
+    assert same_first_last([7]) == True
+    assert same_first_last([]) == False
+    print("All passed!")
+
+def test_first_last6():
+    assert first_last6([1, 2, 6]) == True
+    assert first_last6([6, 1, 2, 3]) == True
+    assert first_last6([13, 6, 1, 2, 3]) == False
+    assert first_last6([13, 6, 1, 2, 6]) == True
+    assert first_last6([3, 2, 1]) == False
+    print("All passed!")
+
+    
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+    test_first_last6()
+    test_same_first_last()
+
